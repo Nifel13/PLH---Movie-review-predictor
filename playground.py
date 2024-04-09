a269aff30f3e506cae54c920be68cf6b7e76012f
Barrenee <barrenecheapereapablo@gmail.com>
2024-04-09 21:30:48 +0200

    deteccion_opinnions1 acabado

--- playground.py
+++ playground.py
@@ -1,9 +1,13 @@
-ll=["FDADFDNAadnkajdf", "DAEJFAEF", "vmkfdnb"]
+import datetime
+from math import sqrt
 
-dd = dict("C":[4], "A": [2])
+t0 = datetime.datetime.now()
 
-for el in dd
+for e in range(10000000):
+    sqrt(e)
 
-ll = map(lambda x: x.lower(), ll)
-ll = [i for i in ll]
-print(ll)
+t1 = datetime.datetime.now()
+time = t1-t0
+time.seconds + time.microseconds
+
+print((t1-t0).microseconds)
